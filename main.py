72dd5dbdd01d52041dadae0a6f3847422c251f6d
Balazs Marton <balazs.marton@prezi.com>
2022-04-08 21:39:15 +0200

    fix: use with for opening file

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ VOTING_RECORD_FILE_PATH = 'voting_record.xml'
 
 def download_voting_data():
     response = requests.get('https://www.europarl.europa.eu/doceo/document/PV-9-2022-03-01-RCV_FR.xml')
-    open(VOTING_RECORD_FILE_PATH, "wb").write(response.content)
+    with open(VOTING_RECORD_FILE_PATH, "wb") as voting_record_file:
+        voting_record_file.write(response.content)
 
 
 def load_voting_data():
