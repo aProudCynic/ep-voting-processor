8e0d21aaa3507c25360438733f28b655127c7209
Balazs Marton <balazs.marton@prezi.com>
2022-04-20 23:52:02 +0200

    chore: create model

---
+++ models.py
@@ -0,0 +1,61 @@
+from typing import (
+    Union,
+    TypeVar,
+    Generic,
+)
+
+from datetime import date
+
+
+class MEP:
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Period:
+    start_date: date
+    end_date: Union[date, None]
+
+    def __init__(self, start_date) -> None:
+        self.start_date = start_date
+        self.end_date = None
+
+
+T = TypeVar('T')
+
+
+class Membership(Generic[T]):
+    member: T
+    membership: Period
+
+    def __init__(self, member, start_date):
+        self.member = member
+        self.membership = Period(start_date)
+
+
+class NationalPartyMembership(Membership[MEP]):
+    pass
+
+
+class NationalParty:
+    name: str
+    members: list[NationalPartyMembership]
+
+    def __init__(self, name):
+        self.name = name
+        self.members = []
+
+
+class EUPoliticalGroupMembership(Membership[Union[NationalParty, MEP]]):
+    pass
+
+
+class EUPoliticalGroup:
+    name: str
+    members: list[EUPoliticalGroupMembership]
+
+    def __init__(self, name):
+        self.name = name
+        self.members = []
