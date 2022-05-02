21b3b739af953448f4056df39f54edc94644ae65
Balazs Marton <balazs.marton@prezi.com>
2022-05-02 22:57:52 +0200

    test: add tests for cohesion

--- test_mep_data_loader.py
+++ test_mep_data_loader.py
@@ -1,5 +1,7 @@
 import unittest
+from collections import Counter
 
+from main import calculate_cohesion
 from mep_data_loader import extract_last_name
 
 
@@ -13,3 +15,15 @@ class TestExtractLastName(unittest.TestCase):
 
     def test_extract_last_name_special_characters(self):
         self.assertEqual(extract_last_name("Ernő SCHALLER-BAROSS"), "SCHALLER-BAROSS")
+
+
+class TestCalculateCohesion(unittest.TestCase):
+
+    def test_total_cohesion(self):
+        self.assertEqual(100.0, calculate_cohesion(Counter({'For': 9, 'Against': 0, 'Abstention': 0})))
+
+    def test_high_cohesion(self):
+        self.assertEqual(90.0, calculate_cohesion(Counter({'For': 9, 'Against': 1, 'Abstention': 0})))
+
+    def test_low_cohesion(self):
+        self.assertEqual(50.0, calculate_cohesion(Counter({'For': 2, 'Against': 4, 'Abstention': 2})))
