00268819fa8fbc3fbec86cee2685d8f9ec5977c2
aProudCynic <bmarton87@gmail.com>
2023-09-28 17:47:02 +0200

    test: find group by name

--- test_mep_data_loader.py
+++ test_mep_data_loader.py
@@ -2,7 +2,8 @@ import unittest
 from collections import Counter
 
 from main import calculate_cohesion
-from mep_data_loader import extract_last_name, extract_national_party_from
+from mep_data_loader import extract_last_name, extract_national_party_from, find_political_group_by_name
+from models import EUPoliticalGroup
 
 
 class TestExtractLastName(unittest.TestCase):
@@ -36,3 +37,32 @@ class TestExtractNationalParty(unittest.TestCase):
 
     def test_extract_national_party_from_party_country(self):
         self.assertEqual("Poland", extract_national_party_from("02-07-2019 / 16-07-2019 : Bezpartyjna (Poland)", "02-07-2019 / 16-07-2019")[1])
+
+class TestFindPoliticalGroupByName(unittest.TestCase):
+
+    epp = EUPoliticalGroup("Group of the European People's Party (Christian Democrats)", ["PPE", "EPP"])
+
+    political_groups = {
+        epp,
+        EUPoliticalGroup("Group of the Progressive Alliance of Socialists and Democrats in the European Parliament", ["S&amp;D", "S&D"]),
+        EUPoliticalGroup("Renew Europe Group", ["Renew"]),
+        EUPoliticalGroup("European Conservatives and Reformists Group", ["ECR"]),
+        EUPoliticalGroup("Group of the Greens/European Free Alliance", ["Verts/ALE", "Greens/EFA"]),
+        EUPoliticalGroup("The Left group in the European Parliament - GUE/NGL", ["The Left", "GUE/NGL"]),
+        EUPoliticalGroup("Identity and Democracy Group", ["ID"]),
+        EUPoliticalGroup("Non-attached Members", ["NI"])
+    }
+
+
+    def test_find_political_group_by_name_positive(self):
+        result = find_political_group_by_name(self.political_groups, "Group of the European People's Party (Christian Democrats)")
+        self.assertEquals(self.epp, result)
+
+
+    def test_find_political_group_by_name_negative(self):
+        try:
+            find_political_group_by_name(self.political_groups, "kiskrumpli")
+        except AssertionError as e:
+            self.assertTrue("No political group named kiskrumpli found", str(e))
+        else:
+            self.fail()
